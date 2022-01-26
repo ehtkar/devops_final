52c8e1f194b8c920b4358c08466d1906e79048b9
Karen Ehret <kehret36@gmail.com>
2022-01-25 19:54:29 -0500

    add testing

--- app/books/tests.py
+++ app/books/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+import pytest
 
 # Create your tests here.
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+@pytest.mark.django_db
+def test_create_book():
+    book = Book.objects.create(
+        title='Pytest',
+        genre='testgenre',
+        author='John Smith',
+    )
+    assert book.title == "Pytest"
